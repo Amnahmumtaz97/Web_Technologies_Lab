4a02da3ea88a4ad189ad824a68bb5c9a3e7c5172
Amnahmumtaz97 <iamnah97@gmail.com>
2025-09-26 12:15:33 +0500

    26sept

---
+++ jquery/script.js
@@ -0,0 +1,11 @@
+//delayed bindings
+//event delegation
+//react 
+
+console.log("script.js loaded");
+function bindings() {
+    console.log("bindings called");
+    let btn = document.getElementById("checkout-btn");
+    
+
+}
