c82e54d579c31de729eb35b22bf9b9f99e2066d3
Amnahmumtaz97 <iamnah97@gmail.com>
2025-10-03 11:01:12 +0500

    crud

--- jquery/script.js
+++ jquery/script.js
@@ -6,6 +6,9 @@ console.log("script.js loaded");
 function bindings() {
     console.log("bindings called");
     let btn = document.getElementById("checkout-btn");
-    
+    btn.addEventListener("click", function() {
+        alert("Checkout button clicked!");
+    });
+    console.log("bindings completed");
 
 }
